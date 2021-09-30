d003468743f0ae1a7975ddb79718deeaf9fd422a
Thauan Amorim <thauanssussu@gmail.com>
2021-09-30 16:51:50 -0300

    add bookRepository

---
+++ backend/src/main/java/com/bookstore/backend/infrastructure/persistence/repository/bookRepository.java
@@ -0,0 +1,12 @@
+package com.bookstore.backend.infrastructure.persistence.repository;
+
+import com.bookstore.backend.domain.model.product.BookModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface bookRepository extends JpaRepository<BookModel, Long> {
+        
+}
+
