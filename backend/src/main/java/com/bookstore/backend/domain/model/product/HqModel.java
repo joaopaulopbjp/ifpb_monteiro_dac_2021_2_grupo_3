349d8c81c39b5e73e90ea660c6c809c8ea95922c
Thauan Amorim <thauanssussu@gmail.com>
2021-09-29 22:32:59 -0300

    adicao construtor para todas as sub-classes

--- backend/src/main/java/com/bookstore/backend/domain/model/product/HqModel.java
+++ backend/src/main/java/com/bookstore/backend/domain/model/product/HqModel.java
@@ -1,4 +1,23 @@
 package com.bookstore.backend.domain.model.product;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.bookstore.backend.domain.model.AuthorModel;
+import com.bookstore.backend.domain.model.CategoryModel;
+import com.bookstore.backend.domain.model.InventoryModel;
+import com.bookstore.backend.domain.model.PublishingCompanyModel;
+import com.bookstore.backend.domain.model.SaleModel;
+
 public class HqModel extends ProductModel{
+
+    public HqModel(Long id, String title, String description, int yearLaunch, int pages, BigDecimal price,
+            SaleModel sale, InventoryModel inventory, List<CategoryModel> categoryList, PublishingCompanyModel company,
+            List<AuthorModel> authorList) {
+        super(id, title, description, yearLaunch, pages, price, sale, inventory, categoryList, company, authorList);
+    }
+
+    public HqModel() {
+    }
+    
 }
--- backend/src/main/java/com/bookstore/backend/domain/model/user/AdminModel.java
+++ backend/src/main/java/com/bookstore/backend/domain/model/user/AdminModel.java
@@ -1,13 +1,24 @@
 package com.bookstore.backend.domain.model.user;
 
+import java.util.List;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
+import com.bookstore.backend.domain.model.AddressModel;
+
 @Entity
 @Table(name = "T_ADMIN")
 public class AdminModel extends PersonModel{
+
+    public AdminModel(Long id, String username, String email, String password, List<AddressModel> addressList) {
+        super(id, username, email, password, addressList);
+    }
+
+    public AdminModel() {
+    }
     
 }
