9da5b3ebf013dd3c0b2b20438d2efceff006735c
Thauan Amorim <thauanssussu@gmail.com>
2021-09-30 16:54:36 -0300

    correcao do nome da classe repository book

--- backend/src/main/java/com/bookstore/backend/infrastructure/persistence/repository/bookRepository.java
+++ backend/src/main/java/com/bookstore/backend/infrastructure/persistence/repository/bookRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface bookRepository extends JpaRepository<BookModel, Long> {
+public interface BookRepository extends JpaRepository<BookModel, Long> {
         
 }
 
