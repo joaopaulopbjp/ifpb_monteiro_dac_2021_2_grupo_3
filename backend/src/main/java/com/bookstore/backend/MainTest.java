e9317a31b8f03e055f8fccac2217b90b2aa3a390
Thauan Amorim <thauanssussu@gmail.com>
2021-09-29 22:15:53 -0300

    teste delete

--- backend/src/main/java/com/bookstore/backend/MainTest.java
+++ backend/src/main/java/com/bookstore/backend/MainTest.java
@@ -4,18 +4,20 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("teste");
         clearConsole();
+        System.out.flush();
         System.out.println("teste2");
     }
     public final static void clearConsole(){
-
+        ProcessBuilder builder;
         try{
             final String os = System.getProperty("os.name");
 
             if (os.contains("Windows")){
-                Runtime.getRuntime().exec("cls");
+                builder = new ProcessBuilder("cmd", "/c", "cls");
+                builder.inheritIO().start().waitFor();
             }
             else{
-                Runtime.getRuntime().exec("clear");
+                Runtime.getRuntime().exec("clear").waitFor();
             }
         }catch (Exception e){
             e.printStackTrace();
