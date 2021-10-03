88034ff039c015bb6833ba3ab0619be04351fc4f
Thauan Amorim <thauanssussu@gmail.com>
2021-10-02 22:19:50 -0300

    import desnecessario

--- backend/src/main/java/com/bookstore/backend/domain/model/user/UserModel.java
+++ backend/src/main/java/com/bookstore/backend/domain/model/user/UserModel.java
@@ -3,7 +3,6 @@ package com.bookstore.backend.domain.model.user;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
