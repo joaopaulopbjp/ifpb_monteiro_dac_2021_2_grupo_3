454d78fe20bc427fcd6f92c0c4cbd19db25275e8
Lucas <lukasqw4@gmail.com>
2021-10-05 17:18:44 -0300

    dor de cabeca

--- backend/src/main/java/com/bookstore/backend/infrastructure/persistence/service/BookRepositoryService.java
+++ backend/src/main/java/com/bookstore/backend/infrastructure/persistence/service/BookRepositoryService.java
@@ -1,5 +1,6 @@
 package com.bookstore.backend.infrastructure.persistence.service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -8,6 +9,8 @@ import com.bookstore.backend.infrastructure.exception.NotFoundException;
 import com.bookstore.backend.infrastructure.persistence.repository.product.BookRepository;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -48,10 +51,22 @@ public class BookRepositoryService {
             throw new NotFoundException();
         }
 
-        ModelMapper mapper = new ModelMapper();
+        BeanUtils.copyProperties(book, bookDataBase, getNullPropertyNames(book));
 
-        mapper.map(book, bookDataBase);
+        return bookRepository.save(bookDataBase);
+    }
 
-        return bookRepository.save(book);
+    public static String[] getNullPropertyNames (Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+    
+        Set<String> emptyNames = new HashSet<String>();
+        for(java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+    
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
     }
 }
