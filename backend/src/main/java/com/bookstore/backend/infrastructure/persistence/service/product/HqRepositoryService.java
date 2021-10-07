6211d7ab4684bd05e771a5c24203e9a91686b903
Thauan Amorim <thauanssussu@gmail.com>
2021-10-06 21:38:01 -0300

    melhorias nos servicos

--- backend/src/main/java/com/bookstore/backend/infrastructure/persistence/service/product/HqRepositoryService.java
+++ backend/src/main/java/com/bookstore/backend/infrastructure/persistence/service/product/HqRepositoryService.java
@@ -15,7 +15,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HqRepositoryService {
     @Autowired
     private HqRepository hqRepository;
