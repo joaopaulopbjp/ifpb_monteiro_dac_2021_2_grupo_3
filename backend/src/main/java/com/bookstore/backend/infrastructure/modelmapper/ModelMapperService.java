20630f19780ace85b757eaf2cb57df69bbb7ef2e
Thauan Amorim <thauanssussu@gmail.com>
2021-10-14 17:22:55 -0300

    correcao do modelmapper

--- backend/src/main/java/com/bookstore/backend/infrastructure/modelmapper/ModelMapperService.java
+++ backend/src/main/java/com/bookstore/backend/infrastructure/modelmapper/ModelMapperService.java
@@ -5,11 +5,11 @@ import org.modelmapper.ModelMapper;
 public class ModelMapperService {
     private static ModelMapper modelMapper;
 
-    public static Object convertToDTO(Object object, Object dtoTarget) {
-        return modelMapper.map(object, dtoTarget.getClass());
+    public static Object convertToDTO(Object object, Class<?> dtoTarget) {
+        return modelMapper.map(object, dtoTarget);
     }
 
-    public static Object convertToModel(Object dto, Object model) {
-        return modelMapper.map(dto, model.getClass());
+    public static Object convertToModel(Object dto, Class<?> model) {
+        return modelMapper.map(dto, model);
     }
 }
