2b8628a78d29742150ef58fd756b39abe9823d5f
0jafc0 <jafclive0@gmail.com>
2021-10-13 22:04:11 -0300

    criado AddressService

---
+++ backend/src/main/java/com/bookstore/backend/application/service/address/AddressService.java
@@ -0,0 +1,42 @@
+package com.bookstore.backend.application.service.address;
+
+import com.bookstore.backend.domain.model.address.AddressModel;
+import com.bookstore.backend.infrastructure.exception.NotFoundException;
+import com.bookstore.backend.infrastructure.persistence.service.address.AddressRepositoryService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressService {
+    
+    @Autowired
+    private AddressRepositoryService addressRepositoryService;
+
+    public AddressModel save(AddressModel address){
+        return addressRepositoryService.getInstance().save(address);
+    }
+
+    public void delete(AddressModel addressModel){
+        addressRepositoryService.getInstance().delete(addressModel);
+    }
+
+    public AddressModel update(AddressModel addressModel) throws NotFoundException{
+        return addressRepositoryService.update(addressModel);
+    }
+
+    public AddressModel findById(Long id) throws NotFoundException{
+        Optional<AddressModel> address = addressRepositoryService.getInstance().findById(id);
+        if(address == null){
+            throw new NotFoundException();
+        }
+        return address.get();
+    }
+
+    public List<AddressModel> findAll(){
+        return addressRepositoryService.getInstance().findAll();
+    }
+}
