e7e9d4d39d956aba273d6fdfcc01b38d36b88a3a
Thauan Amorim <thauanssussu@gmail.com>
2021-10-13 22:32:09 -0300

    inicio do controller book, service completo

---
+++ backend/src/main/java/com/bookstore/backend/application/controller/product/BookController.java
@@ -0,0 +1,24 @@
+package com.bookstore.backend.application.controller.product;
+
+import com.bookstore.backend.application.service.product.BookService;
+import com.bookstore.backend.presentation.dto.product.BookDTO;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/book")
+public class BookController {
+    
+    private BookService bookServices;
+
+    @PostMapping()
+    public ResponseEntity<?> save(@RequestBody BookDTO dto) {
+        return null;
+    }
+}
---
+++ backend/src/main/java/com/bookstore/backend/presentation/dto/product/ProductDTO.java
@@ -0,0 +1,52 @@
+package com.bookstore.backend.presentation.dto.product;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.bookstore.backend.domain.model.author.AuthorModel;
+import com.bookstore.backend.domain.model.category.CategoryModel;
+import com.bookstore.backend.domain.model.company.PublishingCompanyModel;
+import com.bookstore.backend.domain.model.evaluation.EvaluateModel;
+import com.bookstore.backend.domain.model.inventory.InventoryModel;
+import com.bookstore.backend.domain.model.sale.SaleModel;
+import com.bookstore.backend.domain.model.user.PersonModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductDTO {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Integer yearLaunch;
+
+    private Integer pages;
+
+    private BigDecimal price;
+
+    private List<String> imageList;
+
+    private SaleModel sale;
+
+    private InventoryModel inventory;
+    
+    private List<CategoryModel> categoryList;
+
+    private PersonModel saller;
+
+    private PublishingCompanyModel company;
+
+    private List<AuthorModel> authorList;
+
+    private List<EvaluateModel> evaluateList; 
+}
