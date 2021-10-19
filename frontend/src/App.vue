50860bc3019d4645a925076953fa2087bbbad501
Lucas <lukasqw4@gmail.com>
2021-10-19 12:31:22 -0300

    adding Footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
     <Nav-bar/>
+    <br>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
-  components: { NavBar }
+  components: { NavBar , Footer }
 }
 </script>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+    <footer class="footer">
+        <p class="labelLinks">Get connected with us on social networks</p>
+        <div class="line"></div>
+        <p><strong>DAC project &copy; 2021</strong></p>
+        <div class="line"></div>
+        <p class="labelDeveloped"><strong>Developed by:</strong><br><br>João Arthur<br>Lucas Nascimento<br>Thauan Amorim</p>
+
+    </footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+.footer {
+    height: 160px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-top: 50px solid #FCB13A;
+    background: #955ABE;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.line {
+    height: 90px;
+    width: 1px;
+    background: black;
+}
+
+p {
+    padding: 0px 100px;
+    font-size: 18px;
+}
+
+.labelLinks {
+    margin-bottom: 75px;
+}
+
+</style>
