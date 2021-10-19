b3d6ca9d521c024347852851346d7dda8b4fcacb
Lucas <lukasqw4@gmail.com>
2021-10-19 20:02:00 -0300

    standardizing the design fill and margin

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,3 +14,13 @@ export default {
   components: { NavBar , Footer }
 }
 </script>
+
+<style>
+
+* {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  padding: 0;
+  margin: 0;
+  /* box-sizing: border-box; */
+}
+</style>
