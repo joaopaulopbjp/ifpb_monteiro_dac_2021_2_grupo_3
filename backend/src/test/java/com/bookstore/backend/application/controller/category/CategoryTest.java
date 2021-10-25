435eb77dbd6529e2f2381d9fa63b97224ac9508f
Thauan Amorim <thauanssussu@gmail.com>
2021-10-25 16:55:18 -0300

    comeco das correcoes dos tests

--- backend/src/test/java/com/bookstore/backend/application/controller/category/CategoryTest.java
+++ backend/src/test/java/com/bookstore/backend/application/controller/category/CategoryTest.java
@@ -2,38 +2,36 @@ package com.bookstore.backend.application.controller.category;
 
 import static org.mockito.Mockito.when;
 
-import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.bookstore.backend.configs.ConfigClass;
+import com.bookstore.backend.configs.TestsController;
 import com.bookstore.backend.domain.model.category.CategoryModel;
 import com.bookstore.backend.infrastructure.persistence.service.category.CategoryRepositoryService;
-import com.bookstore.backend.presentation.dto.category.CategoryDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = CategoryController.class)
-public class CategoryTest {
-
-    private String URLbase = new ConfigClass().getURLbase();
-
-    @Autowired
-    private MockMvc mockMvc;
+public class CategoryTest extends TestsController {
 
     @MockBean
     private CategoryRepositoryService categoryRepositoryService;
 
     @Test
-    public void saveCategory(){
+    public void saveCategory() throws JsonProcessingException, Exception{
         CategoryModel category = new CategoryModel(0l,"teste");
         when(categoryRepositoryService.getInstance().save(category)).thenReturn(category);
-        this.mockMvc.perform(post(URLbase+"/category"));
+        mockMvc.perform(post(URLbase + "/category")
+            // .header("Authorization", this.getToken())
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(category)))
+            .andExpect(status().isCreated());
     }
     
 }
