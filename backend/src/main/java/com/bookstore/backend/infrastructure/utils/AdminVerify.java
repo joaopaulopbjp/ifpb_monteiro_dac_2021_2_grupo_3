604ca1a7912d06d4745cb39856f9b9786a90ae2f
Thauan Amorim <thauanssussu@gmail.com>
2021-10-31 19:14:45 -0300

    adicao do adminVerify

---
+++ backend/src/main/java/com/bookstore/backend/infrastructure/utils/AdminVerify.java
@@ -0,0 +1,25 @@
+package com.bookstore.backend.infrastructure.utils;
+
+import java.util.Optional;
+
+import com.bookstore.backend.domain.model.user.AdminModel;
+import com.bookstore.backend.infrastructure.persistence.service.person.AdminRepositoryService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminVerify {
+    
+    @Autowired
+    private AdminRepositoryService adminRepositoryService;
+
+    public boolean idAdmin(String username) {
+        Optional<AdminModel> adminOp = adminRepositoryService.getInstance().findByUsername(username);
+
+        if(adminOp.isPresent())
+            return true;
+        return false;
+    }
+
+}
