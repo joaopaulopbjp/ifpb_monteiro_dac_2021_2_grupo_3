3295cd0cdb97937e1fa8111051adc3240855d735
0jafc0 <jafclive0@gmail.com>
2021-11-01 20:38:14 -0300

    corrigido metodo delete

--- backend/src/main/java/com/bookstore/backend/application/service/image/ImageService.java
+++ backend/src/main/java/com/bookstore/backend/application/service/image/ImageService.java
@@ -5,10 +5,13 @@ import java.util.Optional;
 
 import com.bookstore.backend.domain.model.image.ImageModel;
 import com.bookstore.backend.domain.model.product.BookModel;
+import com.bookstore.backend.domain.model.user.UserModel;
 import com.bookstore.backend.infrastructure.exception.FullListException;
 import com.bookstore.backend.infrastructure.exception.NotFoundException;
 import com.bookstore.backend.infrastructure.persistence.service.image.ImageRepositoryService;
+import com.bookstore.backend.infrastructure.persistence.service.person.UserRepositoryService;
 import com.bookstore.backend.infrastructure.persistence.service.product.BookRepositoryService;
+import com.bookstore.backend.infrastructure.utils.AdminVerify;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,24 +25,44 @@ public class ImageService {
     @Autowired
     private BookRepositoryService bookRepositoryService;
 
-    public ImageModel save(ImageModel image, Long idBook) throws FullListException, NotFoundException{
-        Optional<BookModel> book = bookRepositoryService.getInstance().findById(idBook);
-        if(!book.isPresent()){
-            throw new NotFoundException("Not Found Book " + idBook);
+    @Autowired
+    private UserRepositoryService userRepositoryService;
+
+    @Autowired
+    private AdminVerify adminVerify;
+
+    public ImageModel save(ImageModel image, Long idBook, String username) throws FullListException, NotFoundException, Exception{
+        if(!adminVerify.idAdmin(username)){
+
+            Optional<BookModel> book = bookRepositoryService.getInstance().findById(idBook);
+            if(!book.isPresent()){
+                throw new NotFoundException("Not Found Book " + idBook);
+            }
+            book.get().addImageToImageList(image);
+            BookModel bookSaved = bookRepositoryService.getInstance().save(book.get());
+            image = bookSaved.findImageByContent(image.getBase64());
+            return image;
         }
-        book.get().addImageToImageList(image);
-        BookModel bookSaved = bookRepositoryService.getInstance().save(book.get());
-        image = bookSaved.findImageByContent(image.getBase64());
-        return image;
+        throw new Exception("Admins cannot save images");
     }
 
-    public void delete(Long id) throws NotFoundException{
+    public void delete(Long id, String username) throws Exception{
         Optional<ImageModel> image = imageRepositoryService.getInstance().findById(id);
         if(!image.isPresent()){
             throw new NotFoundException("Not found image " + id);
-        }else {
-            imageRepositoryService.getInstance().deleteById(id);
         }
+        if(!adminVerify.idAdmin(username)){
+            Optional<UserModel> userOp = userRepositoryService.getInstance().findByUsername(username);
+            boolean flag = userOp.get().getProductForSaleList().stream().map(product -> product.getImageList())
+                .map(imageList -> imageList.stream()
+                .filter(imageVerify -> imageVerify.getId() == id))
+                .findFirst().isPresent();
+            if(!flag){
+                throw new Exception("You can't delete this image because it belongs to another user");
+            }
+
+        }
+        imageRepositoryService.getInstance().deleteById(id);
     }
 
     public List<ImageModel> findAll() throws NotFoundException{
