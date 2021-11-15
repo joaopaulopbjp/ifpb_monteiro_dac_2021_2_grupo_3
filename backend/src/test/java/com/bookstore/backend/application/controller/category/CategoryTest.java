9e0bf0a04ca5340150733961ea88eaef2fcde295
Thauan Amorim <thauanssussu@gmail.com>
2021-11-15 19:29:52 -0300

    primeiro teste mock para category

--- backend/src/test/java/com/bookstore/backend/application/controller/category/CategoryTest.java
+++ backend/src/test/java/com/bookstore/backend/application/controller/category/CategoryTest.java
@@ -1,39 +1,41 @@
-// package com.bookstore.backend.application.controller.category;
+package com.bookstore.backend.application.controller.category;
 
-// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// import com.bookstore.backend.application.controller.TestsController;
-// import com.bookstore.backend.presentation.dto.category.CategoryDTO;
-// import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.PrintWriter;
 
-// import org.junit.jupiter.api.Disabled;
-// import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.TestInstance;
-// import org.junit.jupiter.api.TestMethodOrder;
-// import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-// import org.junit.jupiter.api.TestInstance.Lifecycle;
-// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-// import org.springframework.boot.test.context.SpringBootTest;
+import javax.servlet.ServletOutputStream;
 
-// @TestInstance(Lifecycle.PER_CLASS)
-// @SpringBootTest
-// @AutoConfigureMockMvc
-// @TestMethodOrder(OrderAnnotation.class)
-// @Disabled
-// public class CategoryTest extends TestsController {
+import com.bookstore.backend.application.controller.TestsController;
+import com.bookstore.backend.presentation.dto.category.CategoryDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
-//     @Test
-//     @Disabled
-//     public void saveCategory() throws JsonProcessingException, Exception{
-//         CategoryDTO dto = new CategoryDTO(null, "ho");
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
+public class CategoryTest extends TestsController {
+
+    @Test
+    public void saveCategory() throws JsonProcessingException, Exception {
+        CategoryDTO dto = new CategoryDTO(0l, "horror");
+
+        JSONObject jsonLogin = new JSONObject(this.login("admin", "admin").getResponse().getContentAsString());
         
-//         mockMvc.perform(post(URLbase + "/category")
-//             // .header("Authorization", this.getToken())
-//             .contentType("application/json")
-//             .content(objectMapper.writeValueAsString(dto)))
-//             .andExpect(status().isCreated())
-//             .andExpect(jsonPath("$.id").value(1));
-//     }
-// }
+        mockMvc.perform(post(URLbase + "/category")
+            .header("Authorization", "Bearer " + jsonLogin.getString("response"))
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(status().isCreated());
+    }
+}
