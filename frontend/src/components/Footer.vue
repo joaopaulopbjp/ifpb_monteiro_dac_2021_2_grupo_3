d52e56c8b9d3a31cc26172af6122355faf86c08c
Lucas <lukasqw4@gmail.com>
2021-11-14 22:08:34 -0300

    alteracao no Footer, mais adicao de links nos icones de linkedin e github

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,10 +1,33 @@
 <template>
-    <footer class="footer">
-        <p class="labelLinks">Get connected with us on social networks</p>
-        <div class="line"></div>
-        <p><strong>DAC project &copy; 2021</strong></p>
-        <div class="line"></div>
-        <p class="labelDeveloped"><strong>Developed by:</strong><br><br>João Arthur<br>Lucas Nascimento<br>Thauan Amorim</p>
+    <footer class="page-footer" style="background-color: #955DBC">
+        <div class="bg-warning">
+            <div class="container">
+                <div class="row py-4 d-flex align-items-center">
+                    <div class="col-md-12 text-center">
+                        <p class="d-inline mr-4">Get connected with us on social networks</p>
+                        <a href="https://docs.google.com/document/d/1E10pyBDxLLp47WjXzN6u13RawzWYb4JZun8VVvFxd9A/edit?usp=sharing" target="_blank"><i class="fab fa-linkedin-in text-dark mr-4 fa-2x"></i></a>
+                        <a href="https://docs.google.com/document/d/1znm7dzXswbS4kfrFsq0cFXhF9iJ-gVOBM3FyNvYgoeA/edit?usp=sharing" target="_blank"><i class="fab fa-github-alt text-dark mr-4 fa-2x"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container text-center text-md-left mt-5">
+            <div class="row">
+
+                <div class="col-md-3 mx-auto mb-4">
+                    <h6 class="text-uppercase font-weight-bold">the developers</h6>
+                    <hr class="bg-warning mb-4 mt-0 d-inline-block mx-auto" style="width: 160px; height: 2px">
+                    <p class="p-2">João Arthur<br>Lucas Nascimento<br>Thauan Amorim</p>
+                </div>
+
+                <div class="col-md-3 mx-auto mb-4">
+                    <h6 class="text-uppercase font-weight-bold">project for purposes</h6>
+                    <hr class="bg-warning mb-4 mt-0 d-inline-block mx-auto" style="width: 225px; height: 2px">
+                    <p class="p-2">This project was developed for academic purposes, and targeted the discipline of DAC</p>
+                </div>
+            </div>
+        </div>
 
     </footer>
 </template>
