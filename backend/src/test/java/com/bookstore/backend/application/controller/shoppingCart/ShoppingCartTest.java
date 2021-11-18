eac7ff73166e479b3cc8812c40435a8f37b31915
0jafc0 <jafclive0@gmail.com>
2021-11-17 21:51:51 -0300

    corrigido erro de constraint, tentando deletar produto que nao deveria

--- backend/src/test/java/com/bookstore/backend/application/controller/shoppingCart/ShoppingCartTest.java
+++ backend/src/test/java/com/bookstore/backend/application/controller/shoppingCart/ShoppingCartTest.java
@@ -32,8 +32,8 @@ public class ShoppingCartTest extends TestsController{
     
     @Test
     @Order(1)
-    public void saveShoppingCart() throws JsonProcessingException, JSONException, UnsupportedEncodingException, Exception{
-        this.saveUser();
+    public void saveShoppingCartSucess() throws JsonProcessingException, JSONException, UnsupportedEncodingException, Exception{        this.saveUser();
+        
 
         ShoppingCartDTO dto = new ShoppingCartDTO();
         
@@ -45,6 +45,73 @@ public class ShoppingCartTest extends TestsController{
         dto.setItemList(list);
         
         mockMvc.perform(post(URLbase + "/shopping-cart/add")
+            .header("Authorization", this.getToken("user", "userPass"))
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(2)
+    public void saveShoppingCartAmountZeroInvalid() throws JsonProcessingException, Exception{
+        this.saveUser();
+
+        ShoppingCartDTO dto = new ShoppingCartDTO();
+        
+        JSONObject jsonBook = new JSONObject(this.saveBook().getResponse().getContentAsString());
+
+        List<ItemOrderDTO> list = new ArrayList<>();
+        list.add(new ItemOrderDTO(0l, 0, null, jsonBook.getLong("id")));
+
+        dto.setItemList(list);
+        
+        mockMvc.perform(post(URLbase + "/shopping-cart/add")
+            .header("Authorization", this.getToken("user", "userPass"))
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @Order(3)
+    public void removeShoppingCartSucess() throws JsonProcessingException, Exception{
+        
+        
+        ShoppingCartDTO dto = new ShoppingCartDTO();
+        
+        JSONObject jsonBook = new JSONObject(this.saveBook().getResponse().getContentAsString());
+        JSONObject jsonUser = new JSONObject(this.saveUser().getResponse().getContentAsString());
+
+        List<ItemOrderDTO> list = new ArrayList<>();
+        list.add(new ItemOrderDTO(0l, 10, null, jsonBook.getLong("id")));
+
+        dto.setItemList(list);
+        dto.setIdPerson(jsonUser.getLong("id"));
+        
+        mockMvc.perform(post(URLbase + "/shopping-cart/remove")
+            .header("Authorization", this.getToken("user", "userPass"))
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(dto)))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(4)
+    public void removeShoppingCartError() throws JsonProcessingException, Exception{
+        
+
+        ShoppingCartDTO dto = new ShoppingCartDTO();
+        
+        JSONObject jsonBook = new JSONObject(this.saveBook().getResponse().getContentAsString());
+        JSONObject jsonUser = new JSONObject(this.saveUser().getResponse().getContentAsString());
+
+        List<ItemOrderDTO> list = new ArrayList<>();
+        list.add(new ItemOrderDTO(0l, 10, null, jsonBook.getLong("id")));
+
+        dto.setItemList(list);
+        dto.setIdPerson(jsonUser.getLong("id"));
+        
+        mockMvc.perform(post(URLbase + "/shopping-cart/remove")
             .header("Authorization", this.getToken("user", "userPass"))
             .contentType("application/json")
             .content(objectMapper.writeValueAsString(dto)))
