6cd61170961b88459efdbed26e36f91301b4de2c
Lucas <lukasqw4@gmail.com>
2021-11-19 16:21:08 -0300

    adicao de testes inventory

--- backend/src/test/java/com/bookstore/backend/application/service/author/AuthorTest.java
+++ backend/src/test/java/com/bookstore/backend/application/service/author/AuthorTest.java
@@ -1,17 +1,22 @@
-package com.bookstore.backend.application.controller.author;
+package com.bookstore.backend.application.service.author;
 
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.bookstore.backend.domain.model.author.AuthorModel;
+import com.bookstore.backend.infrastructure.exception.NotFoundException;
 import com.bookstore.backend.infrastructure.persistence.repository.author.AuthorRepository;
 import com.bookstore.backend.infrastructure.persistence.service.author.AuthorRepositoryService;
 
@@ -21,9 +26,12 @@ public class AuthorTest {
 	
 	@Mock
 	private AuthorRepository authorRepository;
-	
+	@Spy
 	@InjectMocks
 	private AuthorRepositoryService authorRepositoryService;
+	
+	@Mock
+	private AuthorService authorService;
 	
 	@Test
 	@Order(1)
@@ -37,6 +45,27 @@ public class AuthorTest {
 		authorRepositoryService.getInstance().save(model);
 		
 		verify(authorRepository,times(1)).save(model);
+	}
+	
+	
+	@Test
+	@Order(2)
+	public void verifyEmptyNameField() {
+		/*
+		 *Tentativa de Salvar um Author sem nome, estou forçando que a partir do
+		 *when toda vez que for salvar um author sem nome ele lance uma
+		 *IllegalArgumentException
+		 */
+		AuthorModel model = new AuthorModel();
+		
+		when(authorService.save(model)).thenThrow(new IllegalArgumentException());
+		
+		try {
+			authorService.save(model);
+			fail("Deveria ter sido lançado uma [IllegalArgumentException]");
+		} catch (IllegalArgumentException e) {
+			//OK
+		}
 	}
 		
 }
