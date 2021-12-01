883d9200d3f0b3aa674c9a944889e1e6f00af0da
Lucas <lukasqw4@gmail.com>
2021-12-01 17:41:13 -0300

    correcao do botao de adicionar no carrinho da view Home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -8,79 +8,77 @@
         
         <div class="text-center border border-warning mb-3 p-2" style="background: white; width: 23%; border-radius: 5px" >
           <img style="height: 15vw;" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
-           <!-- <img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA
-            AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO
-                9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" /> -->
           <h6 class="mt-2">A garota do lago</h6>
           <b-form-rating class="justify-content-center" id="media" color="#FCB13A" value="4" style="border: none; height: 5vh"/>
-          <div class="d-flex justify-content-center" style="height: 8vh;">
-            <b-button class="btn-sm" variant="dark" style="border-radius: 10px 0px 0px 10px; background-color: #955DBC;">R$: 17,90</b-button>
-            <b-button class="btn-sm " style="border-radius: 0px 10px 10px 0px; background-color: #FCB13A;">ADD TO CART</b-button>
+          <div class="d-flex justify-content-center">
+            <span class="btn-sm p-2 text-white" variant="dark" style="border-radius: 8px 0px 0px 8px; background-color: #955DBC;">R$: 17,90</span>
+            <button style="border-radius: 0px 8px 8px 0px; background-color: #FCB13A; padding: 2px 2px">ADD TO CART</button>
           </div>
         </div>
         <div class="text-center border border-warning ml-3 mb-3 p-2" style="background: white; width: 23%; border-radius: 5px" >
-          <img style="height: 15vw" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
+          <img style="height: 15vw;" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
           <h6 class="mt-2">A garota do lago</h6>
           <b-form-rating class="justify-content-center" id="media" color="#FCB13A" value="4" style="border: none; height: 5vh"/>
-          <div class="d-flex justify-content-center" style="height: 8vh;">
-            <b-button class="btn-sm" variant="dark" style="border-radius: 10px 0px 0px 10px; background-color: #955DBC;">R$: 17,90</b-button>
-            <b-button class="btn-sm " style="border-radius: 0px 10px 10px 0px; background-color: #FCB13A;">ADD TO CART</b-button>
+          <div class="d-flex justify-content-center">
+            <span class="btn-sm p-2 text-white" variant="dark" style="border-radius: 8px 0px 0px 8px; background-color: #955DBC;">R$: 17,90</span>
+            <button style="border-radius: 0px 8px 8px 0px; background-color: #FCB13A; padding: 2px 2px">ADD TO CART</button>
           </div>
         </div>
         <div class="text-center border border-warning ml-3 mb-3 p-2" style="background: white; width: 23%; border-radius: 5px" >
-          <img style="height: 15vw" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
+          <img style="height: 15vw;" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
           <h6 class="mt-2">A garota do lago</h6>
           <b-form-rating class="justify-content-center" id="media" color="#FCB13A" value="4" style="border: none; height: 5vh"/>
-          <div class="d-flex justify-content-center" style="height: 8vh;">
-            <b-button class="btn-sm" variant="dark" style="border-radius: 10px 0px 0px 10px; background-color: #955DBC;">R$: 17,90</b-button>
-            <b-button class="btn-sm " style="border-radius: 0px 10px 10px 0px; background-color: #FCB13A;">ADD TO CART</b-button>
+          <div class="d-flex justify-content-center">
+            <span class="btn-sm p-2 text-white" variant="dark" style="border-radius: 8px 0px 0px 8px; background-color: #955DBC;">R$: 17,90</span>
+            <button style="border-radius: 0px 8px 8px 0px; background-color: #FCB13A; padding: 2px 2px">ADD TO CART</button>
           </div>
         </div>
         <div class="text-center border border-warning ml-3 mb-3 p-2" style="background: white; width: 23%; border-radius: 5px" >
-          <img style="height: 15vw" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
+          <img style="height: 15vw;" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
           <h6 class="mt-2">A garota do lago</h6>
           <b-form-rating class="justify-content-center" id="media" color="#FCB13A" value="4" style="border: none; height: 5vh"/>
-          <div class="d-flex justify-content-center" style="height: 8vh;">
-            <b-button class="btn-sm" variant="dark" style="border-radius: 10px 0px 0px 10px; background-color: #955DBC;">R$: 17,90</b-button>
-            <b-button class="btn-sm " style="border-radius: 0px 10px 10px 0px; background-color: #FCB13A;">ADD TO CART</b-button>
+          <div class="d-flex justify-content-center">
+            <span class="btn-sm p-2 text-white" variant="dark" style="border-radius: 8px 0px 0px 8px; background-color: #955DBC;">R$: 17,90</span>
+            <button style="border-radius: 0px 8px 8px 0px; background-color: #FCB13A; padding: 2px 2px">ADD TO CART</button>
           </div>
         </div>
         <div class="text-center border border-warning mb-3 p-2" style="background: white; width: 23%; border-radius: 5px" >
-          <img style="height: 15vw" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
+          <img style="height: 15vw;" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
           <h6 class="mt-2">A garota do lago</h6>
           <b-form-rating class="justify-content-center" id="media" color="#FCB13A" value="4" style="border: none; height: 5vh"/>
-          <div class="d-flex justify-content-center" style="height: 8vh;">
-            <b-button class="btn-sm" variant="dark" style="border-radius: 10px 0px 0px 10px; background-color: #955DBC;">R$: 17,90</b-button>
-            <b-button class="btn-sm " style="border-radius: 0px 10px 10px 0px; background-color: #FCB13A;">ADD TO CART</b-button>
+          <div class="d-flex justify-content-center">
+            <span class="btn-sm p-2 text-white" variant="dark" style="border-radius: 8px 0px 0px 8px; background-color: #955DBC;">R$: 17,90</span>
+            <button style="border-radius: 0px 8px 8px 0px; background-color: #FCB13A; padding: 2px 2px">ADD TO CART</button>
           </div>
         </div>
         <div class="text-center border border-warning ml-3 mb-3 p-2" style="background: white; width: 23%; border-radius: 5px" >
-          <img style="height: 15vw" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
+          <img style="height: 15vw;" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
           <h6 class="mt-2">A garota do lago</h6>
           <b-form-rating class="justify-content-center" id="media" color="#FCB13A" value="4" style="border: none; height: 5vh"/>
-          <div class="d-flex justify-content-center" style="height: 8vh;">
-            <b-button class="btn-sm" variant="dark" style="border-radius: 10px 0px 0px 10px; background-color: #955DBC;">R$: 17,90</b-button>
-            <b-button class="btn-sm " style="border-radius: 0px 10px 10px 0px; background-color: #FCB13A;">ADD TO CART</b-button>
+          <div class="d-flex justify-content-center">
+            <span class="btn-sm p-2 text-white" variant="dark" style="border-radius: 8px 0px 0px 8px; background-color: #955DBC;">R$: 17,90</span>
+            <button style="border-radius: 0px 8px 8px 0px; background-color: #FCB13A; padding: 2px 2px">ADD TO CART</button>
           </div>
         </div>
         <div class="text-center border border-warning ml-3 mb-3 p-2" style="background: white; width: 23%; border-radius: 5px" >
-          <img style="height: 15vw" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
+          <img style="height: 15vw;" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
           <h6 class="mt-2">A garota do lago</h6>
           <b-form-rating class="justify-content-center" id="media" color="#FCB13A" value="4" style="border: none; height: 5vh"/>
-          <div class="d-flex justify-content-center" style="height: 8vh;">
-            <b-button class="btn-sm" variant="dark" style="border-radius: 10px 0px 0px 10px; background-color: #955DBC;">R$: 17,90</b-button>
-            <b-button class="btn-sm " style="border-radius: 0px 10px 10px 0px; background-color: #FCB13A;">ADD TO CART</b-button>
+          <div class="d-flex justify-content-center">
+            <span class="btn-sm p-2 text-white" variant="dark" style="border-radius: 8px 0px 0px 8px; background-color: #955DBC;">R$: 17,90</span>
+            <button style="border-radius: 0px 8px 8px 0px; background-color: #FCB13A; padding: 2px 2px">ADD TO CART</button>
           </div>
         </div>
         <div class="text-center border border-warning ml-3 mb-3 p-2" style="background: white; width: 23%; border-radius: 5px" >
-          <img style="height: 15vw" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
+          <img style="height: 15vw;" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
           <h6 class="mt-2">A garota do lago</h6>
           <b-form-rating class="justify-content-center" id="media" color="#FCB13A" value="4" style="border: none; height: 5vh"/>
-          <div class="d-flex justify-content-center" style="height: 8vh;">
-            <b-button class="btn-sm" variant="dark" style="border-radius: 10px 0px 0px 10px; background-color: #955DBC;">R$: 17,90</b-button>
-            <b-button class="btn-sm " style="border-radius: 0px 10px 10px 0px; background-color: #FCB13A;">ADD TO CART</b-button>
+          <div class="d-flex justify-content-center">
+            <span class="btn-sm p-2 text-white" variant="dark" style="border-radius: 8px 0px 0px 8px; background-color: #955DBC;">R$: 17,90</span>
+            <button style="border-radius: 0px 8px 8px 0px; background-color: #FCB13A; padding: 2px 2px">ADD TO CART</button>
           </div>
         </div>
+       
         
   
     </div>
