fa00324f42e680dca44873c1e40715e236bc8161
0jafc0 <jafclive0@gmail.com>
2021-12-02 21:27:19 -0300

    trocado os b-card por div

--- frontend/src/views/ShoppingCart.vue
+++ frontend/src/views/ShoppingCart.vue
@@ -4,19 +4,19 @@
     <Side-bar/>
     <div class="container d-flex justify-content-between mt-2 mb-5">
         <div class="container d-grid justify-content-start w-75 h-100" id="itemsDiv">
-            <b-card class="d-grid justify-content-start rounded">
+            <div class="d-grid justify-content-start rounded p-4" style="background-color: white;border-radius: 1px solid black;">
                 <b-form-checkbox>Select all</b-form-checkbox>
-            </b-card>
-            <!-- <b-card class="container rounded mt-2">
+            </div>
+            <div class="container rounded mt-2 p-3" style="background-color: white;border-radius: 1px solid black;">
                 <div class="d-flex justify-content-between">
-                    <b-form-checkbox></b-form-checkbox>
-                    <b-button variant="outline-dark border-0"><i class="far fa-trash-alt w-100 h-100"></i></b-button>
+                    <input type="checkbox" name="" id="" class="fa-2x">
+                    <button variant="outline-dark" class="border-0 fa-2x" style="background-color: rgba(0, 0, 0, 0);border: none;"><i class="far fa-trash-alt w-100 h-100"></i></button>
                 </div>
                 <div class="d-flex justify-content-between">
                     <div class="d-flex justify-content-between">
-                        <router-link class="text-decoration-none text-dark " to="">
+                        <button class="text-decoration-none text-dark " style="background-color: rgba(0, 0, 0, 0);border:none;">
                             <img class="" style="max-width: 120px" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
-                        </router-link>
+                        </button>
                         <div class="d-grid ml-4">
                             <h4>A garota do lago</h4>
                             <h6>Author: Charlie Donlea</h6>
@@ -27,28 +27,7 @@
                     </div>
                     <b-form-spinbutton class="mt-4" id="sb-inline" v-model="quantidade" inline style="background-color: #FCB13A;"></b-form-spinbutton>
                 </div>
-            </b-card>
-            <b-card class="container rounded mt-4">
-                <div class="d-flex justify-content-between">
-                    <b-form-checkbox></b-form-checkbox>
-                    <b-button variant="outline-dark border-0"><i class="far fa-trash-alt w-100 h-100"></i></b-button>
-                </div>
-                <div class="d-flex justify-content-between">
-                    <div class="d-flex justify-content-between">
-                        <router-link class="text-decoration-none text-dark " to="">
-                            <img class="" style="max-width: 120px" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
-                        </router-link>
-                        <div class="d-grid ml-4">
-                            <h4 class="w-10">A garota do lago</h4>
-                            <h6>Author: Charlie Donlea</h6>
-                            <div class="row justify-content-center mt-5 w-10 rounded m-" style="background-color: #9652C6;color: white;align-items: center;">
-                                <h5>R$: 17,90</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <b-form-spinbutton class="mt-4" id="sb-inline" v-model="quantidade" inline style="background-color: #FCB13A;"></b-form-spinbutton>
-                </div>
-            </b-card> -->
+            </div>
         </div>
         <div class="container d-grid w-25 rounded-bottom sticky">
             <div class="row rounded-top w-100 mb-0 pl-4 pt-3" style="background-color: white;">
