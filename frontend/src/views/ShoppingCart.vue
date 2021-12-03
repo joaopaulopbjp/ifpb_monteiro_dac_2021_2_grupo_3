aac60ef67ab293f5f9dc884831a1b5aa49361968
0jafc0 <jafclive0@gmail.com>
2021-12-03 20:53:22 -0300

    merged

--- frontend/src/views/ShoppingCart.vue
+++ frontend/src/views/ShoppingCart.vue
@@ -7,33 +7,37 @@
             <div class="d-grid justify-content-start rounded p-4" style="background-color: white;border-radius: 1px solid black;">
                 <b-form-checkbox>Select all</b-form-checkbox>
             </div>
-            <div id="itemsDiv">
-                <!-- <div class="container rounded mt-2 p-3" style="background-color: white;border-radius: 1px solid black;">
+            <div id="itemsDiv" class="container rounded mt-2 p-3" style="background-color: white;border-radius: 1px solid black;">
+                <div class="d-flex justify-content-between p-1">
+                    <input type="checkbox" name="" id="" style=" width: 2vw;height: 2vh;">
+                    <button variant="outline-dark" class="border-0 far fa-trash-alt" style="background-color: rgba(0, 0, 0, 0);border: none;font-size: 3vh"/>
+                </div>
+                <div class="d-flex justify-content-between">
                     <div class="d-flex justify-content-between">
-                        <input type="checkbox" name="" id="" class="fa-2x">
-                        <button variant="outline-dark" class="border-0 fa-2x" style="background-color: rgba(0, 0, 0, 0);border: none;"><i class="far fa-trash-alt w-100 h-100"></i></button>
-                    </div>
-                    <div class="d-flex justify-content-between">
-                        <div class="d-flex justify-content-between">
-                            <button class="text-decoration-none text-dark " style="background-color: rgba(0, 0, 0, 0);border:none;">
-                                <img class="" style="max-width: 120px" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000" alt="">
-                            </button>
-                            <div class="d-grid ml-4">
-                                <h4>A garota do lago</h4>
-                                <h6>Author: Charlie Donlea</h6>
-                                <div class="row justify-content-center mt-5 w-10 rounded" style="background-color: #9652C6;color: white;align-items: center;">
-                                    <h5>R$: 17,90</h5>
-                                </div>
+                        <button class="text-decoration-none text-dark " style="background-color: rgba(0, 0, 0, 0);border:none;">
+                            <img class="" style="max-width: 120px" src="https://lojasaraiva.vteximg.com.br/arquivos/ids/12109069/1006574337.jpg?v=637142248039070000">
+                        </button>
+                        <div class="d-grid ml-4">
+                            <h4>A garota do lago</h4>
+                            <h6>Author: Charlie Donlea</h6>
+                            <div class="row justify-content-center mt-5 w-10 rounded" style="background-color: #9652C6;color: white;align-items: center;">
+                                <h5>R$: 17,90</h5>
                             </div>
                         </div>
                         <b-form-spinbutton class="mt-4" id="sb-inline" v-model="quantidade" inline style="background-color: #FCB13A;"></b-form-spinbutton>
                     </div>
-                </div> -->
+                </div> 
 
+                    <div class=" mt-4" inline >
+                        <button class="text-center buttonMinus rounded-left">-</button>
+                        <input class="text-center rounded inputMeio" type="number" min="1" value="1" >
+                        <button class="text-center buttonPlus rounded-right">+</button>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="container d-grid w-25 rounded-bottom sticky" id="containerValue">
-            <!-- <div class="row rounded-top w-100 mb-0 pl-4 pt-3" style="background-color: white;">
+             <div class="row rounded-top w-100 mb-0 pl-4 pt-3" style="background-color: white;">
                 <div class="">
                     <h4>Order:</h4>
                     <h5>Subtotal: R$: 0,0</h5>
@@ -42,9 +46,8 @@
                     <h5>Total: R$: 0,0</h5>
                 </div>
             </div>
-            <button class="row d-flex justify-content-center rounded-bottom w-100" style="background-color: #FCB13A;border: none;color: white;">buy</button> -->
+            <button class="row d-flex justify-content-center p-1 rounded-bottom w-100" style="background-color: #FCB13A;border: none;color: white;">buy</button>
         </div>
-    </div>
     <Footer/>
   </div>
 </template>
@@ -55,6 +58,32 @@
         top: 1%;
         width: 10px;
         height: 10px;
+    }
+
+    .buttonMinus,.buttonPlus{
+        width: 2vw;
+    }
+    .buttonPlus{
+        background-color: #FCB13A;
+        border: none;
+        color:white;
+    }
+    .inputMeio{
+        border: 2px solid #FCB13A;
+        width: 3vw;
+    }
+    .buttonMinus{
+        background-color: #FCB13A;
+        border: none;
+        color:white;
+    }
+    .inputMeio:focus{
+        outline: none;
+
+    }
+    .inputMeio::-webkit-inner-spin-button{
+        -webkit-appearance: none;
+        margin: 0;
     }
 </style>
 
@@ -83,7 +112,6 @@ export default {
             compras:[
                 { text: 'Lewis', value: 'Lewis' }
             ],
-            quantidade: 1,
         }
     },
     mounted() {
