ac07e0dd2ae1d238d7e27d013ac9447916abb0fa
0jafc0 <jafclive0@gmail.com>
2021-12-06 15:43:12 -0300

    alterado alguns textos e adicionado select para escolha do produto

--- frontend/src/views/UpdateProduct.vue
+++ frontend/src/views/UpdateProduct.vue
@@ -5,41 +5,49 @@
         <div class="container mt-2 mb-5">
             <b-card>
                 <div class="d-flex justify-content-between ml-5"><h1>Update a Book</h1></div>
-                <div class="d-flex justify-content-start mb-4">
-                    <b-form-input type="text" placeholder="New Title" id="newTitleInput" style="width: 65%" class="ml-3"></b-form-input>
-                    <b-form-input type="number" placeholder="New YearLaunch" id="newYearLaunchInput" style="width: 40%" class="ml-2"></b-form-input>
-                    <b-form-input type="number" placeholder="New Page" id="newPageInput" style="width: 40%" class="ml-2 mr-3"></b-form-input>
-                </div>
-                <div class="d-flex justify-content-start mb-4">
-                    <b-form-input type="number" placeholder="New Price" id="newPriceInput" style="width: 30%" class="ml-3"></b-form-input>
-                    <b-form-input type="number" placeholder="New Inventory" id="newInventoryInput" style="width: 40%" class="ml-2"></b-form-input>
-
-                    <select id="newCompanyInput" style="width: 45%" class="ml-2 companys">
-                        <option selected disabled value="">Select New a company</option>
+                <b-card class="align-items-center">
+                    <select id="selectProduct" class="p-2">
+                        <option selected disabled value="">Choose the product to update</option>
                     </select>
+                </b-card>
+                <b-card class="mt-4">
+                    <div class="d-flex justify-content-start mb-4">
+                        <b-form-input type="text" placeholder="New Title" id="newTitleInput" style="width: 65%" class="ml-3"></b-form-input>
+                        <b-form-input type="number" placeholder="New YearLaunch" id="newYearLaunchInput" style="width: 40%" class="ml-2"></b-form-input>
+                        <b-form-input type="number" placeholder="New Page" id="newPageInput" style="width: 40%" class="ml-2 mr-3"></b-form-input>
+                    </div>
+                    <div class="d-flex justify-content-start mb-4">
+                        <b-form-input type="number" placeholder="New Price" id="newPriceInput" style="width: 30%" class="ml-3"></b-form-input>
+                        <b-form-input type="number" placeholder="New Inventory" id="newInventoryInput" style="width: 40%" class="ml-2"></b-form-input>
 
-                    <b-form-file accept="image/jpeg, image/png" id="newimageInput" style="width: 45%" class="ml-2 mr-3"></b-form-file>
-                    
-                </div>
-                <div class="d-flex justify-content-between mb-4">
-                    <b-card  class="ml-3 mr-3 w-45 p-3">
-                        <h4 class="text-center">Authors</h4>
-                        <div class="checkboxWidth" id="newAuthorsOptions">
-                        </div>
-                    </b-card>
+                        <select id="newCompanyInput" style="width: 45%" class="ml-2 companys">
+                            <option selected disabled value="">Select New a company</option>
+                        </select>
 
-                    <b-form-textarea placeholder="New Description" id="newDescriptionInput" class="md-textarea p-3" style="width: 45%; "></b-form-textarea>
+                        <b-form-file accept="image/jpeg, image/png" id="newimageInput" style="width: 45%" class="ml-2 mr-3"></b-form-file>
+                        
+                    </div>
+                    <div class="d-flex justify-content-between mb-4">
+                        <b-card  class="ml-3 mr-3 w-45 p-3">
+                            <h4 class="text-center">Authors</h4>
+                            <div class="checkboxWidth" id="newAuthorsOptions">
+                            </div>
+                        </b-card>
 
-                    <b-card class="ml-3 mr-3 w-45 p-3">
-                        <h4 class="text-center">Categories</h4>
-                        <div class="checkboxWidth" id="newCategoriesOptions">
-                        </div>
-                    </b-card>
-                </div>
-                <div class="d-flex justify-content-center">
-                    <b-button to="Profile" variant="danger" class="mt-4 mr-2">Cancel</b-button>
-                    <b-button variant="warning" class="mt-4" id="updateButton">Update Book</b-button>
-                </div>
+                        <b-form-textarea placeholder="New Description" id="newDescriptionInput" class="md-textarea p-3" style="width: 45%; "></b-form-textarea>
+
+                        <b-card class="ml-3 mr-3 w-45 p-3">
+                            <h4 class="text-center">Categories</h4>
+                            <div class="checkboxWidth" id="newCategoriesOptions">
+                            </div>
+                        </b-card>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <b-button to="Profile" variant="danger" class="mt-4 mr-2">Cancel</b-button>
+                        <b-button variant="warning" class="mt-4" id="updateButton">Update Book</b-button>
+                    </div>
+                </b-card>
+
             </b-card>
         </div>
         <Footer/>
