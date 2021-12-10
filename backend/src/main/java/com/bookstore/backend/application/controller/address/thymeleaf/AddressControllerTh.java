5ba3abe54422c4a17fc2bccb6cd63d11334a3479
Lucas <lukasqw4@gmail.com>
2021-12-10 15:34:49 -0300

    implementacao AddressControllerTh quebrada

--- backend/src/main/java/com/bookstore/backend/application/controller/address/thymeleaf/AddressControllerTh.java
+++ backend/src/main/java/com/bookstore/backend/application/controller/address/thymeleaf/AddressControllerTh.java
@@ -1,5 +1,14 @@
 package com.bookstore.backend.application.controller.address.thymeleaf;
 
+import com.bookstore.backend.application.service.address.AddressService;
+
+@RestController
+@RequestMapping("/api/address")
+@CrossOrigin
 public class AddressControllerTh {
       
+      @Autowired
+      private AddressService addressService;
+
+      public String nameView (H)
 }
