d397c2692a0b62c63837004cf5bff1f62d097efd
Thauan Amorim <thauanssussu@gmail.com>
2021-12-10 16:30:45 -0300

    quase funcionando

--- backend/src/main/java/com/bookstore/backend/application/controller/address/thymeleaf/AddressControllerTh.java
+++ backend/src/main/java/com/bookstore/backend/application/controller/address/thymeleaf/AddressControllerTh.java
@@ -1,14 +1,32 @@
 package com.bookstore.backend.application.controller.address.thymeleaf;
 
-import com.bookstore.backend.application.service.address.AddressService;
+import java.security.Principal;
 
-@RestController
-@RequestMapping("/api/address")
+import javax.servlet.http.HttpServletRequest;
+
+import com.bookstore.backend.application.service.address.AddressService;
+import com.bookstore.backend.infrastructure.exception.NotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/addressTh")
 @CrossOrigin
+@Controller
 public class AddressControllerTh {
       
       @Autowired
       private AddressService addressService;
 
-      public String nameView (H)
+      @GetMapping("/hello")
+      public String nameView(Model model, Principal principal) {
+            try {
+                  model.addAttribute("addressList", addressService.findAll(principal.getName()));
+            } catch (NotFoundException e) {}
+            return "hello";
+      }
 }
