0ac375dc28d49a15680edf6c052e05b2b53a9964
0jafc0 <jafclive0@gmail.com>
2021-12-11 19:32:01 -0300

    comecado implementacao do modal registerAddress com thymeleaf

--- backend/src/main/java/com/bookstore/backend/application/controller/address/thymeleaf/AddressControllerTh.java
+++ backend/src/main/java/com/bookstore/backend/application/controller/address/thymeleaf/AddressControllerTh.java
@@ -5,7 +5,10 @@ import java.security.Principal;
 import javax.servlet.http.HttpServletRequest;
 
 import com.bookstore.backend.application.service.address.AddressService;
+import com.bookstore.backend.domain.model.address.AddressModel;
 import com.bookstore.backend.infrastructure.exception.NotFoundException;
+import com.bookstore.backend.infrastructure.modelmapper.ModelMapperService;
+import com.bookstore.backend.presentation.dto.address.AddressDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +16,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RequestMapping("/api/addressTh")
 @CrossOrigin
@@ -29,4 +35,16 @@ public class AddressControllerTh {
             } catch (NotFoundException e) {}
             return "address";
       }
+
+      @PostMapping(value="/register")
+      public String register(@RequestBody AddressDTO dto,Principal principal) {
+            try {
+                  AddressModel model = (AddressModel) ModelMapperService.convertToModel(dto, AddressModel.class);
+                  addressService.save(model, principal.getName());
+            } catch (NotFoundException e) {
+                  e.printStackTrace();
+            }
+            return "salvo";
+      }
+      
 }
