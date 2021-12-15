10163e728aa93e60cd6be5afd2ab42b83e474cdc
Thauan Amorim <thauanssussu@gmail.com>
2021-12-15 15:40:29 -0300

    save author on base

--- backend/src/test/java/com/bookstore/backend/testSystem/Base.java
+++ backend/src/test/java/com/bookstore/backend/testSystem/Base.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Base {
       protected WebDriver driver;
@@ -16,12 +15,12 @@ public class Base {
       protected void instanciaDriver(){
           System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
           this.driver = new ChromeDriver();
+          driver.get("http://localhost:8081/");
+          waitScreen(2000);
           
       }
 
       protected void autoLogin(String username, String password) {
-          driver.get("http://localhost:8081/");
-          waitScreen(2000);
           driver.findElement(By.xpath("/html/body/div/div/div[1]/div/nav/ul/li[2]/a/button")).click();
           waitScreen(500);
           driver.findElement(By.xpath("/html/body/div/div/div[3]/div/div[2]/div/div/div/button")).click();
@@ -35,6 +34,27 @@ public class Base {
           driver.findElement(By.xpath("/html/body/div/div/div[4]/div[3]/button[2]")).click();
 
           waitScreen(1000);
+      }
+
+      protected void autoSaveAuthor(String name) {
+        waitScreen();
+        WebElement sideBarButton = driver.findElement(By.xpath("/html/body/div/div/div[1]/div/nav/ul/li[2]/a/button"));
+        sideBarButton.click();
+
+        waitScreen(2000);
+        WebElement profileButton = driver.findElement(By.xpath("/html/body/div/div/div[3]/div/div[2]/div/div/div/a"));
+        profileButton.click();
+
+        waitScreen();
+        WebElement registerButton = driver.findElement(By.xpath("/html/body/div/div/div[15]/div/div/div[2]/div[2]/div/button[1]"));
+        registerButton.click();
+        waitScreen();
+        
+        WebElement nameInput = driver.findElement(By.xpath("/html/body/div/div/div[3]/div/input"));
+        nameInput.sendKeys(name);
+
+        driver.findElement(By.xpath("/html/body/div/div/div[3]/div/button[2]")).click();
+        waitScreen(1000);
       }
 
       protected void waitScreen() {
--- backend/src/test/java/com/bookstore/backend/testSystem/product/ProductTest.java
+++ backend/src/test/java/com/bookstore/backend/testSystem/product/ProductTest.java
@@ -2,13 +2,22 @@ package com.bookstore.backend.testSystem.product;
 
 import com.bookstore.backend.testSystem.Base;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class ProductTest extends Base {
     
     @Test
-    public void ProductTest() {
+    @Order(1)
+    public void ProductTestSucess() {
         autoLogin("admin", "admin");
+
+        autoSaveAuthor("Luquinha da rolimã");
     }
 
 }
