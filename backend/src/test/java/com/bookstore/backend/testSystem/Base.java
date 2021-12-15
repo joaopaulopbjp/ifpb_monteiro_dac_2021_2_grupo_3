7db3a40c60cad090d60d32dafc30af28b1327cc8
Thauan Amorim <thauanssussu@gmail.com>
2021-12-14 22:04:29 -0300

    autoLogin on Base

--- backend/src/test/java/com/bookstore/backend/testSystem/Base.java
+++ backend/src/test/java/com/bookstore/backend/testSystem/Base.java
@@ -1,14 +1,39 @@
 package com.bookstore.backend.testSystem;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Base {
       protected WebDriver driver;
 
+      public Base() {
+          instanciaDriver();
+      }
+
       protected void instanciaDriver(){
           System.setProperty("webdriver.gecko.driver", "./driver/geckodriver.exe");
           this.driver = new FirefoxDriver();
+          
+      }
+
+      protected void autoLogin(String username, String password) {
+          driver.get("http://localhost:8081/");
+          waitScreen(2000);
+          driver.findElement(By.xpath("/html/body/div/div/div[1]/div/nav/ul/li[2]/a/button")).click();
+          waitScreen(500);
+          driver.findElement(By.xpath("/html/body/div/div/div[3]/div/div[2]/div/div/div/button")).click();
+          waitScreen(500);
+
+          WebElement usernameInput = driver.findElement(By.xpath("/html/body/div/div/div[4]/div[3]/input"));
+          usernameInput.sendKeys(username);
+          WebElement passwordInput = driver.findElement(By.xpath("/html/body/div/div/div[4]/div[3]/form/input"));
+          passwordInput.sendKeys(password);
+
+          driver.findElement(By.xpath("/html/body/div/div/div[4]/div[3]/button[2]")).click();
+
+          waitScreen(1000);
       }
 
       protected void waitScreen() {
