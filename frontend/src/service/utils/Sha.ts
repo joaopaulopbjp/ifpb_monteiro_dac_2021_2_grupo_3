fc6760c94936c9142b2f197bc1d98ea82f01e39f
Thauan Amorim <thauanssussu@gmail.com>
2021-12-17 14:37:02 -0300

    nova pasta thymeleaf

--- frontend/src/service/utils/Sha.ts
+++
@@ -1,9 +0,0 @@
-class Sha {
-    sha256 = (message: string) => {
-        const encoder = new TextEncoder();
-        const data = encoder.encode(message);
-        return window.crypto.subtle.digest('SHA-256', data);
-    }
-}
-
-export { Sha };
