8496c943743c7d392693b91d39d222a62280b371
Thauan Amorim <thauanssussu@gmail.com>
2021-12-21 20:32:44 -0300

    logout button

--- thymeleaf/backend/src/main/java/com/bookstore/backend/infrastructure/security/auth/SecurityConfiguration.java
+++ thymeleaf/backend/src/main/java/com/bookstore/backend/infrastructure/security/auth/SecurityConfiguration.java
@@ -58,13 +58,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         http.authorizeRequests()
         	.antMatchers("/api/thymeleaf/address").permitAll()
         	.antMatchers("/api/thymeleaf/address/form-address").permitAll()
-            .antMatchers(HttpMethod.POST,"/api/thymeleaf/address/save").hasAuthority("ADMIN")
+            .antMatchers(HttpMethod.POST,"/api/thymeleaf/address/save").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin()
             .loginPage("/login").permitAll()
-            .defaultSuccessUrl("/api/thymeleaf/address");
-            // .logout(logout -> logout.logoutUrl("/logout"))
+            .defaultSuccessUrl("/api/thymeleaf/address")
+            .and()
+            .logout();
             
             // .and()
             
